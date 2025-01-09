fec741d84b56067aa3793e1323481ff3afdbd1fa
Jose Carlos <josecarlos.alvarez@planeta.com.mx>
2025-01-09 15:58:08 -0600

    Creacion de archivos para mostrar en react

--- reactfront/src/App.js
+++ reactfront/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <BrowserRouter>
+        </BrowserRouter>
       </header>
     </div>
   );
